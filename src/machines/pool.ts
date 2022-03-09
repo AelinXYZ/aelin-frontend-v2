7cb09c1a03c1df2e9d270d73f1d794fca2fecee0
luchobonatti <lucho.bonatti@gmail.com>
2022-03-09 14:28:34 -0300

    Create pool machine

--- src/machines/pool.ts
+++ src/machines/pool.ts
@@ -1,124 +1,113 @@
-import { assign, /* interpret, */ createMachine } from 'xstate'
+import { assign, createMachine } from 'xstate'
 
-export interface XStateWeb3ReactContext {
-  deltme: any
+type Address = string
+
+export interface poolContext {
+  // Currency used to purchase pool tokens
+  purchaseToken: Address | null
+  // max number of pool token: 0 = uncapped or specific amount
+  purchaseTokenCap: number
+  // The amount of time purchasers have to purchase pool tokens
+  purchaseExpiry: Date | null
+  // The amount of time a sponsor has to find a deal before purchaser can withdraw their funds
+  investmenDealLine: Date | null
+  // the paid fee to the sponsor
+  sponsorFee: number
+  poolName: string | null
+  poolSymbol: string | null
+  isPrivate: boolean
 }
 
 // export enum Event {
-//   PROVIDER = "PROVIDER",
-//   ERROR = "ERROR",
-//   CONNECT = "CONNECT",
-//   ON_WALLET_UPDATED = "ON_WALLET_UPDATED",
-//   DISCONNECT = "DISCONNECT",
+
 // }
-
 // type ProviderEvent = { type: Event.PROVIDER; payload: { provider: any } };
-// type ErrorEvent = { type: Event.ERROR };
-// type ConnectEvent = { type: Event.CONNECT };
-// type OnWalletUpdatedEvent = { type: Event.ON_WALLET_UPDATED };
-// type DisconnectEvent = { type: Event.DISCONNECT };
 
-export const xStateWeb3Model =
-  /** @xstate-layout N4IgpgJg5mDOIC5QHcwCMDMB1AhgGzzABcBhAewDsKwBjIgS0oDoAhAJzOVjDYEkL6RAMSJQABzKxBjCqJAAPRAFoArAEYA7Ew0YATAA4M+gGz79GlfpUAaEAE9lxlQAYmagwBY1ATg-ONxt4YAL7BtqiYuATE5FS0DMyx1HT0FFBCAAoASgDyAGq8ACIAollyElIJskgKyj4YTN5W5obq5pYetg4ISmrGWioaGt7eGiZN+t6DoeHo2PiEpJTJVUxJ8anppbllNRXSlHKKCBjOKkzOumoYPqPXzqZdyroeutoqphhfH2rOvhozEARebRJZxFLMDIcABu9AgPHWdEgTBwYjEiIYaREe0kB2qoGOvSsTBUGGMuheujJVg0uieCDUZgu5j8tN8Hm8ag8HkBwKiiwxMiYULIsPhbAxyNRYgAcmQwStIEJKPziABVMQQHBEMDlXFVI7KV7eNxDZy-U5nU50+yIYZMTxqJ0jUwWaZhIFzVUKjaQmFwhHLeJStGSiDKijejVanV6yoyQ0IIIXMkaP5NNOk3TGen24z1fzXAw3YzGXlehYxIMQijC-3isMo0PVnXh4rQsAUIgAAgwcbxiY8xO8V28pn0zm52Ye9M0DR0fQeRg8-X85cilZ9NbrooDEpbSsKvAAyiQcjKZcUSAAVfsGmrHDC0km3EaU8nmzq2hAeB1nFzDOmZJDmWHp8pugrMLg0hpAAYmQ+7glUQhnheV63ji8aHA+iD6G8KhjmcpgfC8GAqF+3RKE+bzkiYeFFsYNwvKEHoUGQ8LwDU4GgpBtbsJw3B8AIRB3gmOE9J8jRNB4GDctaKjkfSqhGG4xYEYEagfBY64ggKLZCoKaSidhBJ1M+ZgrppHwTr4jJKda2g+AEXz6EORikjp3q8TuYqBkhIbovpRmYQO4m9DoTB+GOaZkk+9H0hOjmyYYHJ6MYzhfJ5EH6X6u4NgeEBNrK8phsZ+K1D0HguI0Bhpq8vz6PcGj0nov4uORKhUoxVkhGBFY8TltYir5iGKoV0qlSF96mQghhMLJZFPq6+a+Dm366IMJLmkMK6dfm1xZQNSFCsNe6TeI+piTNSgeBov4juYk5XM4jWnM137GA6rmaVMk4jv0nKHXpx1QTgMFQPBo2+uV+zTRVXxqEwgQySuY5BL4pJKTJJqkmmniySukw8n1G5HSsQqpLARALN6ZWJko-QOrdhgWmRhgZUpjH6PNfiaLoDzmEMAIk7pVYgzDl0mRVShploIyuQT8mKd+MtUqprn8-+f29aEQA */
-  createMachine(
+const newMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QAcD2qA2A6AYgVwDsIBLAqAYlQPyNKgGENVZJEVniAXYqtkAD0QAmAJwBWLAA4hYgAwBGAGyLZYgOySRaxQBoQAT0QAWI7KyK1AZiNy1sy7NmTFAXxd60mXIRJlKBABEwNFguPhCuHgI+QQQ1IywjeRlZRUl5OzSRPUMEBwSxIVlReUKhDKKjNw90bAB1AENIshxUACcghox-TowAZTwAIwBbLk5WJBAI7l5J2LShLBFHBVMxS0V5eSMcxAczOySNWW1kyQ3qqdqsRuaoVo6wLvIAYyYWAAVa8I4Z6LnEOdJEttOohOdFCJFEJdAZjCosJYhODLGolGoxFV3FcvLduC12gAJTAQMCPaavd5gL6YH6hP4xRBbEyI1JCSwbRRGCwaXZ5cpLMSlQpOVKSExiS6eepNfH3IkkslBClUOpcAAWEDaDQA7nTIrNQLEROClmyiuy1FDCnz5CJgZjUWJNql4qUsTVcbK6A9iRhSeTfv4AIIvF7BTi9fUMgEIE3yKSoowY2RQ+07OFxswiEROqzO1KWe1S654n0K-1K4JBqgAJUgYGGyD+fU4DTanGjUUZCGZCQc0I5ym5al5mfkSKw4OWahh0mURTUJa9d19isD9P8NIwAFEiBN2PTu7G+6zB5yR2PcjIRFgMopClohDYtvFl9ht4xmJB-GrOJrtT1SZpmPI0mTESQzGSNEizEdYILUPkNjUJZTCMGFYM2UR3ywVBkDAAh61JJs-i3Wo918KAu0NARwPUcwjBzOCH0kTFR1tSdpyceJlFUZ83GxAhUFJeBgOuGhKOo-4wIQZM+SOLATnsZkZCsDEPRxGVV3aKMxKPGjYiMTQp3kSQMSyDRMWceT0MUrlVHQh8inEHCywJNo-QDZVfiknszMWCxGOhaFig2SQkK2RTc3U1i3RnHDPypCBfJPaxgSwl9RHOVRYVyEwE3ZE4TCMSw5HFc4cLwgiiMbZtQKufTpNo3s0vMZJMvtBxnQ4yxzCtSEOXEIpNA06UsAANTgOUUpkioEjUkxp1MO0+RkBNnWkFENGfSxxXfGbmvZW1gRWRwklTNERAnASXCAA */
+  createMachine<poolContext>(
     {
-      schema: {
-        context: {} as XStateWeb3ReactContext,
-        // events: {} ,
+      context: {
+        purchaseToken: null,
+        purchaseTokenCap: 0,
+        purchaseExpiry: null,
+        investmenDealLine: null,
+        sponsorFee: 0,
+        poolName: null,
+        poolSymbol: null,
+        isPrivate: false,
       },
-      id: 'web3WalletConnection',
-      initial: 'BrowserInit',
+      id: 'pool',
+      initial: 'Funding',
       states: {
-        BrowserInit: {
-          always: [
-            {
-              cond: 'noWeb3Detected',
-              target: '#web3WalletConnection.installWallet',
-            },
-            {
-              cond: 'storedConnection',
-              target: '#web3WalletConnection.Connecting',
-            },
-            {
-              target: '#web3WalletConnection.WaitingForConnection',
-            },
-          ],
-        },
-        Connecting: {
-          //   on: {
-          //     [Event.PROVIDER]: {
-          //       actions: "addProviderToContext",
-          //       target: "#web3WalletConnection.ProviderConnected",
-          //     },
-          //     [Event.ERROR]: {
-          //       target: "#web3WalletConnection.WaitingForConnection",
-          //     },
-          //   },
-        },
-        ProviderConnected: {
-          initial: 'appConnecting',
-          states: {
-            appConnecting: {
-              always: [
-                {
-                  cond: 'accountIsReady',
-                  target: '#web3WalletConnection.ProviderConnected.appConnected',
-                },
-                {
-                  target: '#web3WalletConnection.ProviderConnected.appNotConnected',
-                },
-              ],
-            },
-            appNotConnected: {
-              //   on: {
-              //     [Event.ON_WALLET_UPDATED]: {
-              //       target: "#web3WalletConnection.ProviderConnected.appConnecting",
-              //     },
-              //   },
-            },
-            appConnected: {
-              //   on: {
-              //     [Event.ON_WALLET_UPDATED]: {
-              //       target: "#web3WalletConnection.ProviderConnected.appConnecting",
-              //     },
-              //   },
-            },
-          },
+        Funding: {
+          entry: 'checkConditions',
           on: {
-            // [Event.DISCONNECT]: {
-            //   target: "#web3WalletConnection.WaitingForConnection",
-            // },
+            onFundingClosed: {
+              target: '#pool.WaitingForDeal',
+            },
+            onDeposit: {
+              actions: 'increaseFunding',
+              target: '#pool.Funding',
+            },
           },
         },
-        WaitingForConnection: {
-          //   on: {
-          //     [Event.CONNECT]: {
-          //       target: '#web3WalletConnection.Connecting',
-          //     },
-          //   },
+        WaitingForDeal: {
+          on: {
+            onDealSubmitted: {
+              target: '#pool.WaitingForHolderDeposit',
+            },
+            closePool: {
+              target: '#pool.PoolClosed',
+            },
+          },
         },
-        installWallet: {
+        WaitingForHolderDeposit: {
+          on: {
+            closePool: {
+              target: '#pool.PoolClosed',
+            },
+            onWithdraw: {
+              actions: 'rejectDeal',
+              target: '#pool.WaitingForHolderDeposit',
+            },
+            onAcceptDeal: {
+              actions: 'acceptDeal',
+              target: '#pool.WaitingForHolderDeposit',
+            },
+            onRedemptionStart: {
+              target: '#pool.openRedemption',
+            },
+            onPoolEnded: {
+              target: '#pool.Vesting',
+            },
+          },
+        },
+        PoolClosed: {
           type: 'final',
+          on: {
+            onWithdraw: {
+              actions: 'updatePoolStatus',
+              target: '#pool.PoolClosed',
+            },
+          },
         },
+        openRedemption: {
+          on: {
+            onPoolEnding: {
+              target: '#pool.Vesting',
+            },
+          },
+        },
+        Vesting: {},
       },
     },
     {
-      actions: {
-        // addProviderToContext: assign({
-        //   provider: (_context, event: Pick<ProviderEvent, "payload">) => event.payload.provider,
-        // }),
-      },
-      guards: {
-        // noWeb3Detected: () => {
-        //   return typeof window === "undefined" || window.ethereum === "undefined";
-        // },
-        // storedConnection: (context, event) => {
-        //   return false;
-        //   // check if there is a stored connection
-        // },
-        // accountIsReady: (context, event) => {
-        //   return true;
-        // },
-      },
+      // Global machine actions (using in events)
+      actions: {},
+      services: {},
+      guards: {},
     },
   )
+
+export default newMachine
