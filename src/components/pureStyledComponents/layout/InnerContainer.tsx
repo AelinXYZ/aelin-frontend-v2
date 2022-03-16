94a7adbab0bcd5b0f1983446b2cb97d80948a0e5
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-03-16 17:04:19 -0300

    (fix) component width

--- src/components/pureStyledComponents/layout/InnerContainer.tsx
+++ src/components/pureStyledComponents/layout/InnerContainer.tsx
@@ -5,7 +5,6 @@ import { ContainerPadding } from '@/src/components/pureStyledComponents/layout/C
 export const InnerContainer = styled.div`
   display: flex;
   flex-direction: column;
-  flex-grow: 1;
   flex-shrink: 0;
   margin: 0 auto;
   max-width: 100vw;
