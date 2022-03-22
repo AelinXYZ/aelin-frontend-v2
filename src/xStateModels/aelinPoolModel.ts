c899251359a8d13872597317956a4b6c93a6b47c
nicosampler <nf.dominguez.87@gmail.com>
2022-03-22 14:36:18 -0300

    xstate & poolInfo

---
+++ src/xStateModels/aelinPoolModel.ts
@@ -0,0 +1,83 @@
+import { ContextFrom, EventObject } from 'xstate'
+import { createModel } from 'xstate/lib/model'
+
+import { ParsedAelinPool } from '@/src/hooks/useAelinPool'
+
+function assertEvent<TEvent extends EventObject, Type extends TEvent['type']>(
+  ev: TEvent,
+  type: Type,
+): asserts ev is Extract<TEvent, { type: Type }> {
+  if (ev.type !== type) {
+    throw new Error('Unexpected event type.')
+  }
+}
+
+const model = createModel(
+  {
+    pool: {} as ParsedAelinPool,
+  },
+  {
+    events: {
+      POOL_UPDATED: (pool: ParsedAelinPool) => ({ pool }),
+      APPROVE: (value: number) => ({ value }),
+      DEPOSIT: (value: number) => ({ value }),
+      DEAL: (deal: any) => ({ deal }),
+    },
+  },
+)
+
+function guardWaitingForDeal(context: ContextFrom<typeof model>) {
+  console.log('guardWaitingForDeal')
+  return false
+}
+
+export const aelinPoolMachine = model.createMachine(
+  {
+    initial: 'funding',
+    context: model.initialContext,
+    states: {
+      funding: {
+        always: [{ target: 'waitingForDeal', cond: guardWaitingForDeal.name }],
+        on: {
+          APPROVE: [
+            {
+              target: 'funding',
+              //cond: 'isValidMove',
+              //actions: 'updateBoard',
+            },
+          ],
+          POOL_UPDATED: {
+            actions: 'updatePool',
+          },
+        },
+      },
+      waitingForDeal: {
+        on: {
+          DEAL: {
+            target: 'closed',
+          },
+        },
+      },
+      closed: {
+        type: 'final',
+      },
+    },
+  },
+  {
+    actions: {
+      updatePool: model.assign({
+        pool: (_context, event) => {
+          assertEvent(event, 'POOL_UPDATED')
+          return event.pool
+        },
+      }),
+      //   resetGame: model.reset(),
+      //   setWinner: model.assign({
+    },
+    guards: {
+      guardWaitingForDeal,
+      //checkDraw,
+      //isValidMove,
+    },
+  },
+)
