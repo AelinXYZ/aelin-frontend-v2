d253f6059a2696931d511b1c2481d9d878c3e39d
luchobonatti <lucho.bonatti@gmail.com>
2022-03-23 13:25:10 -0300

    Add missing columns information in pools list

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -1,2 +1,11 @@
+import { PoolStatus } from '@/graphql-schema'
+
 // 20 pool results by chain
 export const POOLS_RESULTS_PER_CHAIN = 20
+
+export const STATUS_TEXT = {
+  [PoolStatus.PoolOpen]: 'Pool Open',
+  [PoolStatus.DealOpen]: 'Deal',
+  [PoolStatus.FundingDeal]: 'Funding Deal',
+  default: 'Unknown',
+}
