a362765c486a1c84ae9338663247d6816a3cda9a
sebaspado <sebaspado@gmail.com>
2022-03-25 10:34:11 -0300

    delete unnused imports

--- src/constants/myPools.tsx
+++ src/constants/myPools.tsx
@@ -1,8 +1,6 @@
-import { History } from '@/src/components/assets/History'
 import { Pools } from '@/src/components/assets/Pools'
 import { Sponsors } from '@/src/components/assets/Sponsors'
 import { Stake } from '@/src/components/assets/Stake'
-import { Vest } from '@/src/components/assets/Vest'
 
 export const myPools = [
   {
