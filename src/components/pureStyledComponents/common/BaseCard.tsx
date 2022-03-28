eb87b9e2ae1833e100d183f11ff56c57924b0a74
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-03-28 15:54:07 -0300

    (fix) header's width

--- src/components/pureStyledComponents/common/BaseCard.tsx
+++ src/components/pureStyledComponents/common/BaseCard.tsx
@@ -5,7 +5,6 @@ export const BaseCardCSS = css`
   border-radius: ${({ theme }) => theme.card.borderRadius};
   border: ${(props) => props.theme.card.borderColor};
   padding: 20px 20px;
-  width: 100%;
 `
 
 export const BaseCard = styled.div`
