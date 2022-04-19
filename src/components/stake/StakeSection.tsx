21479b9da14e794e973fd6112b1e491e10f6f334
saeta.eth <saeta@aelindao.xyz>
2022-04-19 01:49:37 -0300

    Add StakeInfo component

--- src/components/stake/StakeSection.tsx
+++ src/components/stake/StakeSection.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import styled from 'styled-components'
 
 import ClaimBox from './ClaimBox'
+import StakeInfo from './StakeInfo'
 import StakeTabContent from './StakeTabContent'
 import { DEPOSIT_TYPE, WITHDRAW_TYPE } from './kind'
 import { genericSuspense } from '@/src/components/helpers/SafeSuspense'
@@ -24,26 +25,6 @@ const Wrapper = styled(BaseCard)`
   display: flex;
   align-items: center;
   flex-direction: column;
-`
-
-const InfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  padding: 20px;
-  min-height: 120px;
-`
-
-const Value = styled.span`
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-const Text = styled.p`
-  color: ${({ theme }) => theme.colors.textColorLight};
-  font-size: 1.4rem;
-  font-weight: 400;
-  line-height: 1.2;
-  margin: 5px;
 `
 
 type StakingRewardsResponse = GelatoStakingResponse | AelinStakingResponse | UniswapStakingResponse
@@ -108,32 +89,7 @@ const StakeSection: FC<StakeSectionProps> = ({
           />
         </Tab>
       </Tabs>
-      <InfoWrapper>
-        {isPool2 && (
-          <>
-            <Text>
-              ETH in pool via G-UNI: <Value>{`${rewards?.ethInPool?.toFixed(2) ?? 0}`}</Value>
-            </Text>
-            <Text>
-              Aelin in pool via G-UNI: <Value>{`${rewards?.aelinInPool?.toFixed(2) ?? 0}`}</Value>
-            </Text>
-            <Text>
-              My stake: <Value>{`${rewards?.userStake ?? 0} ${rewards?.symbol}`}</Value>
-            </Text>
-          </>
-        )}
-        {!isPool2 && (
-          <>
-            <Text>
-              Aelin staking:{' '}
-              <Value>{`${rewards?.totalAelinStaked?.toFixed(2) ?? 0} ${rewards?.symbol}`}</Value>
-            </Text>
-            <Text>
-              My stake: <Value>{`${rewards?.userStake ?? 0} ${rewards?.symbol}`}</Value>
-            </Text>
-          </>
-        )}
-      </InfoWrapper>
+      <StakeInfo isPool2={isPool2} rewards={rewards} />
       <ClaimBox stakingAddress={stakingAddress} userRewards={rewards?.userRewards ?? 0} />
     </Wrapper>
   )
