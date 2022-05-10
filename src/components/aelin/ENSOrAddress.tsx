1ad982beee8f0c0a039ce2bb91e19c136faf41ad
luchobonatti <lucho.bonatti@gmail.com>
2022-05-10 15:17:06 -0300

    Add working ENSOrAddress component using useEnsName hook

--- src/components/aelin/ENSOrAddress.tsx
+++ src/components/aelin/ENSOrAddress.tsx
@@ -1,13 +1,24 @@
-import { getAddress } from '@ethersproject/address'
+import { isAddress } from '@ethersproject/address'
 
+import { genericSuspense } from '../helpers/SafeSuspense'
+import { ExternalLink } from '../table/ExternalLink'
+import { ChainsValues } from '@/src/constants/chains'
+import { useEnsLookUpAddress } from '@/src/hooks/useEnsName'
 import { shortenAddress } from '@/src/utils/string'
 
 type Props = {
   address: string
+  network: ChainsValues
 }
 
-export default function ENSOrAddress({ address }: Props) {
-  const _address = getAddress(address)
+const ENSOrAddress = ({ address, network }: Props) => {
+  const { data, explorerUrl } = useEnsLookUpAddress(address, network)
 
-  return <>{shortenAddress(_address)}</>
+  if (!data) return null
+
+  return (
+    <ExternalLink href={explorerUrl}>{isAddress(data) ? shortenAddress(data) : data}</ExternalLink>
+  )
 }
+
+export default genericSuspense(ENSOrAddress, () => null)
---
+++ src/hooks/useEnsName.tsx
@@ -0,0 +1,59 @@
+import { useCallback } from 'react'
+
+import { JsonRpcProvider } from '@ethersproject/providers'
+import useSWR from 'swr'
+
+import { Chains, ChainsValues, getNetworkConfig } from '../constants/chains'
+import { useWeb3Connection } from '../providers/web3ConnectionProvider'
+
+const { rpcUrl } = getNetworkConfig(Chains.mainnet)
+
+const mainnetRpcProvider = new JsonRpcProvider(rpcUrl)
+
+export const useEnsLookUpAddress = (address: string, network: ChainsValues) => {
+  const { getExplorerUrl } = useWeb3Connection()
+
+  const { data, isValidating } = useSWR(
+    mainnetRpcProvider && address ? ['ensLookUpAddress', address] : null,
+
+    async () => {
+      try {
+        const ens = await mainnetRpcProvider.lookupAddress(address)
+        if (!ens) throw new Error(`No ens 'address'}`)
+        return ens
+      } catch (err) {
+        return address
+      }
+    },
+    {
+      refreshWhenHidden: false,
+      revalidateOnFocus: false,
+      revalidateOnMount: false,
+      revalidateOnReconnect: false,
+      refreshWhenOffline: false,
+    },
+  )
+
+  return {
+    data,
+    isValidating,
+    explorerUrl: getExplorerUrl(address, network),
+  }
+}
+
+const isValidENSName = (str: string) => str.length > 3 && str.includes('.')
+
+export const ensResolver = async (name: string) => {
+  if (isValidENSName(name)) {
+    try {
+      const ens = await mainnetRpcProvider.resolveName(name)
+      console.log(ens)
+      if (!ens) throw new Error(`No ens 'address'}`)
+      return ens
+    } catch (err) {
+      console.log(err)
+      return name
+    }
+  }
+  return name
+}
