f959a3c541194718c52b4e65a7c68ec375bad384
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-05-11 15:17:10 -0300

    (add) ENS / address loading

--- src/components/aelin/ENSOrAddress.tsx
+++ src/components/aelin/ENSOrAddress.tsx
@@ -1,24 +1,48 @@
+import styled, { keyframes } from 'styled-components'
+
 import { isAddress } from '@ethersproject/address'
 
-import { genericSuspense } from '../helpers/SafeSuspense'
-import { ExternalLink } from '../table/ExternalLink'
+import { genericSuspense } from '@/src/components/helpers/SafeSuspense'
+import { ExternalLink } from '@/src/components/table/ExternalLink'
 import { ChainsValues } from '@/src/constants/chains'
 import { useEnsLookUpAddress } from '@/src/hooks/useEnsName'
 import { shortenAddress } from '@/src/utils/string'
 
-type Props = {
+const loadingAnimation = keyframes`
+  0% {
+    opacity: 0.6;
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0.6;
+  }
+`
+
+const Loading = styled.div`
+  animation-delay: 0;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-name: ${loadingAnimation};
+  animation-timing-function: ease-in-out;
+  color: ${({ theme }) => theme.colors.textLight};
+  font-style: italic;
+`
+
+const ENSOrAddress: React.FC<{
   address: string
   network: ChainsValues
-}
-
-const ENSOrAddress = ({ address, network }: Props) => {
+}> = ({ address, network, ...restProps }) => {
   const { data, explorerUrl } = useEnsLookUpAddress(address, network)
 
-  if (!data) return null
-
-  return (
-    <ExternalLink href={explorerUrl}>{isAddress(data) ? shortenAddress(data) : data}</ExternalLink>
-  )
+  return data ? (
+    <ExternalLink {...restProps} href={explorerUrl}>
+      {isAddress(data) ? shortenAddress(data) : data}
+    </ExternalLink>
+  ) : null
 }
 
-export default genericSuspense(ENSOrAddress, () => null)
+export default genericSuspense(ENSOrAddress, () => <Loading>Loading...</Loading>)
