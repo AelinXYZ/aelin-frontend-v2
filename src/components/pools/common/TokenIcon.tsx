03102f92b2ac8ba216dc31b13202de47d9355b8b
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-05-17 22:55:08 -0300

    (fix) icon size

--- src/components/pools/common/TokenIcon.tsx
+++ src/components/pools/common/TokenIcon.tsx
@@ -1,42 +1,67 @@
 import Image from 'next/image'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-import { ExternalLink } from '@/src/components/table/ExternalLink'
+import { ExternalLink as BaseExternalLink } from '@/src/components/table/ExternalLink'
 import { ChainsValues } from '@/src/constants/chains'
 import { useTokenIcons } from '@/src/providers/tokenIconsProvider'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
 
-const TokenIconWrapper = styled.div<{
-  direction: 'column' | 'row'
-}>`
+type Direction = 'column' | 'row'
+
+const Wrapper = styled.div<{ direction?: Direction }>`
   align-items: center;
+  column-gap: 6px;
   display: flex;
-  justify-content: center;
-  text-align: center;
   flex-direction: ${({ direction }) => direction};
   font-size: 13px;
-
-  > :last-child {
-    padding-left: ${({ direction }) => (direction === 'row' ? '10px' : 0)};
-    padding-top: ${({ direction }) => (direction === 'column' ? '3px' : 0)};
-  }
+  justify-content: center;
+  row-gap: 3px;
+  text-align: center;
 `
+
+Wrapper.defaultProps = {
+  direction: 'row',
+}
+
+const ExternalLink = styled(BaseExternalLink)<{ direction?: Direction }>`
+  color: ${({ theme }) => theme.colors.textColor};
+  font-size: ${({ direction }) => (direction === 'row' ? 'inherit' : '1rem')};
+  gap: 6px;
+
+  ${({ direction }) =>
+    direction === 'column' &&
+    css`
+      > svg {
+    height: 8px;
+  width: 8px;
+      margin-top: -1px;
+    `}};
+
+`
+
+ExternalLink.defaultProps = {
+  direction: 'row',
+}
+
 interface Props {
-  symbol: string
   address: string
   network: ChainsValues
-  type: 'column' | 'row'
+  symbol: string
+  type?: Direction
 }
-export const TokenIcon: React.FC<Props> = ({ address, network, symbol, type }) => {
+
+export const TokenIcon: React.FC<Props> = ({ address, network, symbol, type = 'column' }) => {
   const { tokens: tokensBySymbol } = useTokenIcons()
   const { getExplorerUrl } = useWeb3Connection()
   const investmentTokenImage = tokensBySymbol[symbol.toLowerCase()]?.logoURI
 
   return investmentTokenImage ? (
-    <TokenIconWrapper direction={type}>
+    <Wrapper direction={type}>
       <Image alt={symbol} height={18} src={investmentTokenImage} title={symbol} width={18} />
-      <ExternalLink href={getExplorerUrl(address, network)}>{symbol}</ExternalLink>
-    </TokenIconWrapper>
+      <ExternalLink direction={type} href={getExplorerUrl(address, network)}>
+        {symbol}
+      </ExternalLink>
+    </Wrapper>
   ) : (
     <>
       <ExternalLink href={getExplorerUrl(address, network)}>{symbol}</ExternalLink>
