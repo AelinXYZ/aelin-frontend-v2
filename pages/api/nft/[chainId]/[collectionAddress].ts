7dfbff1a967809d7b34c83a0798c2fae3bc74bf8
saeta.eth <saeta@aelindao.xyz>
2022-07-29 01:33:59 +0200

    fix import

--- pages/api/nft/[chainId]/[collectionAddress].ts
+++ pages/api/nft/[chainId]/[collectionAddress].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { getNftCollectionDataController } from '@/src/controllers/nft'
+import { getNftOwnedByAddressController } from '@/src/controllers/nft'
 
 export enum Method {
   GET = 'GET',
@@ -10,7 +10,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req
   switch (method) {
     case Method.GET:
-      await getNftCollectionDataController(req, res)
+      await getNftOwnedByAddressController(req, res)
       break
     default:
       return res.status(400).json({ success: false })
