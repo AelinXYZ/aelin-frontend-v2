3e515dda1cd9f56d0bab4c9f69d216fd8d42ad0c
saeta.eth <saeta@aelindao.xyz>
2022-10-13 16:20:30 -0500

    Add script to generate a sample csv

---
+++ scripts/generate-csv-for-merkle-tree.ts
@@ -0,0 +1,56 @@
+// Note: Add "type": "module" to the package.json before running the script
+
+import { Wallet } from '@ethersproject/wallet'
+import fs from 'fs/promises'
+import json2csv from 'json-2-csv'
+
+const MAX_WALLETS = 10000
+const MAX_ALLOCATION = 100000
+const MIN_ALLOCATION = 1
+
+type Rows = {
+  address: string
+  allocation: number
+}
+
+const createCSVFile = async (rows: Rows[]) => {
+  const csv = await json2csv.json2csvAsync(rows)
+
+  return fs.appendFile(
+    `${process.cwd()}/public/data/csv-for-merkel-tree/csv-for-merkel-tree.csv`,
+    csv,
+  )
+}
+
+const main = async () => {
+  console.log('Starting')
+
+  const rows = []
+
+  for (let x = 0; x < MAX_WALLETS; x++) {
+    const randomWallet = Wallet.createRandom()
+    const randomAllocation = Math.round(
+      Math.random() * (MAX_ALLOCATION - MIN_ALLOCATION) + MIN_ALLOCATION,
+    )
+
+    const row = {
+      address: randomWallet.address,
+      allocation: randomAllocation,
+    }
+
+    rows.push(row)
+  }
+
+  return createCSVFile(rows)
+}
+
+main()
+  .then(() => {
+    console.log('CSV has been created successfully')
+    process.exit(0)
+  })
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  .catch((e: any) => {
+    console.error(e)
+    process.exit(1)
+  })
