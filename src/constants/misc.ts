8e52a289468a6a1a98620fa38ac974e0be36c35c
saeta.eth <saeta@aelindao.xyz>
2022-11-02 23:21:24 -0300

    Hide a pool

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -11,7 +11,6 @@ export const HIDDEN_POOLS = [
   '0x97fc4e0ce415ef922b08f4725a0fa197d7fdbec3',
   '0x1beb0c0af60c037aa5b7e48b2e5cd952fe512390',
 ]
-
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const WAD_DECIMALS = 18
 export const WAIT_BLOCKS = 8
