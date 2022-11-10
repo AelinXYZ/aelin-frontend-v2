ef8425a97d51815bba569c522384771dc9e9f64c
saeta.eth <saeta@aelindao.xyz>
2022-11-10 16:14:33 -0300

    Direct deal fixes

--- src/hooks/aelin/useAelinCreateUpFrontDeal.tsx
+++ src/hooks/aelin/useAelinCreateUpFrontDeal.tsx
@@ -668,6 +668,7 @@ export default function useAelinCreateDeal(chainId: ChainsValues) {
 
     return dispatch({ type: 'updateStep', payload: value })
   }
+
   const handleCreateUpFrontDeal = async () => {
     const [upFrontDealData, upFrontDealConfig, nftCollectionRules, allowListAddresses] =
       await parseValuesToCreateUpFrontDeal(
@@ -731,7 +732,12 @@ export default function useAelinCreateDeal(chainId: ChainsValues) {
 
           try {
             const receipt = await execute(
-              [upFrontDealDataFull, upFrontDealConfig, nftCollectionRules, emptyAllowlistData],
+              [
+                upFrontDealDataFull,
+                upFrontDealConfig,
+                formattedNftCollectionRules,
+                emptyAllowlistData,
+              ],
               txGasOptions,
             )
 
@@ -761,7 +767,7 @@ export default function useAelinCreateDeal(chainId: ChainsValues) {
 
           try {
             const receipt = await execute(
-              [upFrontDealData, upFrontDealConfig, nftCollectionRules, allowListAddresses],
+              [upFrontDealData, upFrontDealConfig, formattedNftCollectionRules, allowListAddresses],
               txGasOptions,
             )
 
