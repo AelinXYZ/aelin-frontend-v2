4caf59344bf2a70a9bd63418e557124719697371
James <jamest.1402@gmail.com>
2023-02-23 01:19:32 +1100

    fix blur effect

--- src/components/pureStyledComponents/common/BaseCard.tsx
+++ src/components/pureStyledComponents/common/BaseCard.tsx
@@ -5,7 +5,18 @@ export const BaseCardCSS = css`
   border-radius: 8px;
   border: ${({ theme: { card } }) => card.borderColor};
   padding: 20px;
-  backdrop-filter: ${({ theme: { card } }) => card.backdropFilter};
+  position: relative;
+
+  /* pseudo element to apply background blur - applying directly doesn't render correctly.. */
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: ${({ theme: { card } }) => card.backdropFilter};
+    -webkit-backdrop-filter: ${({ theme: { card } }) => card.backdropFilter};
+    z-index: -1;
+  }
 `
 
 export const BaseCard = styled.div`
