4c855980660f2be93ec17261f980f7a3bb68dd3a
Linuz <linus@aelindao.xyz>
2023-02-28 17:54:39 +0100

    added goerli tokens

--- src/constants/token.ts
+++ src/constants/token.ts
@@ -35,21 +35,27 @@ export const TestnetTokens: { [chainId: number]: Token[] } = {
   [Chains.goerli]: [
     {
       name: 'Wrapped Ether',
-      address: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+      address: (process.env.NEXT_PUBLIC_ANVIL as string)
+        ? (process.env.NEXT_PUBLIC_WETH as string)
+        : '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
       symbol: 'WETH',
       decimals: BASE_DECIMALS,
       chainId: Chains.goerli,
     },
     {
       name: 'Uniswap',
-      address: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+      address: (process.env.NEXT_PUBLIC_ANVIL as string)
+        ? (process.env.NEXT_PUBLIC_UNI as string)
+        : '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
       symbol: 'UNI',
       decimals: BASE_DECIMALS,
       chainId: Chains.goerli,
     },
     {
       name: 'USDC',
-      address: '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C',
+      address: (process.env.NEXT_PUBLIC_ANVIL as string)
+        ? (process.env.NEXT_PUBLIC_USDC as string)
+        : '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C',
       symbol: 'USDC',
       decimals: 6,
       chainId: Chains.goerli,
