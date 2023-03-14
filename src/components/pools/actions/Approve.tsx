15fb9d550d8b0dd73f11c66f4ac56fcaa2f7428c
0xDmitry <sign-69@mail.ru>
2023-03-14 20:36:50 +0300

    Add error message when holder does not have enough to fund the deal (approve action)

--- src/components/pools/actions/Approve.tsx
+++ src/components/pools/actions/Approve.tsx
@@ -28,6 +28,7 @@ type Props = {
   approveAmt?: BigNumber
   allowance?: string
   symbol?: string
+  noEnoughBalance?: boolean
 }
 
 const Allowance = ({ allowance, symbol }: { allowance: string; symbol: string }) => (
@@ -48,6 +49,7 @@ export default function Approve({
   tokenAddress,
   approveAmt = MAX_BN,
   allowance,
+  noEnoughBalance,
 }: Props) {
   const { address, appChainId, isAppConnected } = useWeb3Connection()
 
@@ -78,13 +80,16 @@ export default function Approve({
       <Contents>{description}</Contents>
       <ButtonsWrapper>
         <ButtonGradient
-          disabled={!address || !isAppConnected || isSubmitting || !userBalance?.gt(0)}
+          disabled={
+            !address || !isAppConnected || isSubmitting || noEnoughBalance || !userBalance?.gt(0)
+          }
           onClick={approveInvestmentToken}
         >
           Approve
         </ButtonGradient>
       </ButtonsWrapper>
-      {!userBalance?.gt(0) && symbol && (
+      {noEnoughBalance && <BaseError textAlign="center">Insufficient balance</BaseError>}
+      {!noEnoughBalance && !userBalance?.gt(0) && symbol && (
         <BaseError textAlign="center">You must have {symbol} in your wallet</BaseError>
       )}
     </>
